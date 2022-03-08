289b3451686a37d439f18e79b82a53da48144908
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2022-03-08 14:06:40 +0100

    Td4

---
+++ Td4/Program.cs
@@ -0,0 +1,15 @@
+﻿using ServiceReference1;
+
+namespace TD4
+{
+
+    internal class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var client = new ServiceReference1.CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+            Console.WriteLine(await client.ChannelFactory.CreateChannel().AddAsync(73, 30));
+        }
+    }
+}
