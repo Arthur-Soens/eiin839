699fe23d8f7b99791ef597aa496e6cd01bf4f86c
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2022-03-15 14:31:19 +0100

    TD5

---
+++ TD5/ClientWSDL/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using ServiceReference1;
+
+class Program
+{
+
+    static async Task Main(string[] arg)
+    {
+        {
+            ServiceReference1.MathsOperationsClient client = new MathsOperationsClient();
+
+            int result = await  client.AddAsync(3, 4);
+            Console.WriteLine(result);
+            result = await client.SubstractAsync(3, 4);
+            Console.WriteLine(result);
+            result = await client.MultiplyAsync(3, 4);
+            Console.WriteLine(result);
+            float resultD = await client.DivideAsync(3, 4);
+            Console.WriteLine(resultD);
+
+            // Utilisez la variable 'client' pour appeler des opérations sur le service.
+
+            // Fermez toujours le client.
+            client.Close();
+        }
+    }
+}
