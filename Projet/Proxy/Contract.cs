d88d367d9eee24a445fb788d8cf2c2e14e32f53a
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2022-03-28 21:09:26 +0200

    Start implementing proxy with cache

---
+++ Projet/Proxy/Contract.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Proxy
+{
+    public class Contract
+    {
+        public string name { get; set; }
+        public string commercial_name { get; set; }
+        public List<string> cities { get; set; }
+        public string country_code { get; set; }
+
+        public string toString()
+        {
+            if (cities == null)
+            {
+                return "Name : " + name + " Commercial : " + commercial_name + " Country code : " + country_code;
+            }
+            return "Name : " + name + ", Commercial : " + commercial_name + ", Country code : " + country_code + ", Cities : " + string.Join(";", cities) + "\n";
+        }
+    }
+}
---
+++ Projet/Proxy/Proxy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Caching;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class Proxy : IProxy
+    {
+        public async Task<List<Contract>> GetContracts(int value)
+        {
+            ObjectCache cache = MemoryCache.Default;
+            var expirationTime = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(30.0),
+            };
+            if (cache.Get("Contracts") != null)
+            {
+                return (List<Contract>)cache.Get("Contracts");
+            }
+            HttpClient clientSocket = new HttpClient();
+            HttpResponseMessage response = await clientSocket.GetAsync("https://api.jcdecaux.com/vls/v3/contracts?apiKey=2ba463e0d63cedfd5374762396b92c89cd41ec62");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            List<Contract> list = JsonSerializer.Deserialize<List<Contract>>(responseBody);
+            cache.Set("Contracts", list, expirationTime);
+            Console.WriteLine("Not from cache");
+            return list;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
